71a96c6a22a624dcd0fdc957413e73e693eb2c0e
imyoubin <imyoubin@hanmail.net>
2025-05-02 15:50:26 +0900

    2025.05.02

--- src/com/javaex/ex20/ColorPoint.java
+++ src/com/javaex/ex20/ColorPoint.java
@@ -1,5 +1,50 @@
 package com.javaex.ex20;
 
-public class ColorPoint {
+public class ColorPoint extends Point{
 
+	//필드
+	private String color;
+
+	//생성자
+	public ColorPoint() {
+		System.out.println("ColorPoint()");
+	}
+	public ColorPoint(String color) {
+		
+		this.color = color;
+		System.out.println("ColorPoint(1)");
+	}
+	
+	 public ColorPoint(int x, int y, String color) {
+	        super(x, y);               
+	        this.color = color;        
+	        System.out.println("ColorPoint(3)");
+		
+	}
+	 //////////////////////////////////////
+	//메소드gs
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	//메소드 일반
+	@Override
+	public String toString() {
+		return "ColorPoint [color=" + color + "]";
+	}
+	
+	public void showInfo() {
+		System.out.println(super.getX());
+		System.out.println();
+		System.out.println();
+		
+	}
+
+	
+	
+	
+	
+	
 }
--- src/com/javaex/ex20/PointApp.java
+++ src/com/javaex/ex20/PointApp.java
@@ -4,6 +4,14 @@ public class PointApp {
 	
 	public static void main(String[] args) {
 		
+		Point p = new Point(4,4);
+		ColorPoint cp1 = new ColorPoint("red");
+		ColorPoint cp2 = new ColorPoint(10,10,"blue");
+		
+		System.out.println(p.getX());
+		System.out.println(cp1.getColor());
+		System.out.println(cp2.getColor());
+		
 		
 	}
 }
